6a7273df67856980f5512a109c225b4c04474f9c
Juanmgrado <jumi.rc@hotmail.com>
2024-11-20 17:25:10 +0100

    funcion para cargar imgs modificada para aceptar mas de 1 file

--- back/src/cloudinary/cloudinary.service.ts
+++ back/src/cloudinary/cloudinary.service.ts
@@ -5,38 +5,47 @@ import { Readable } from 'stream';
 @Injectable()
 export class CloudinaryService {
 
-    async upLoadImg(file: Express.Multer.File): Promise<string> {
-        
-        try {
-
-          const allowedMimeTypes = ['image/jpeg', 'image/png', 'image/gif'];
-          if (!allowedMimeTypes.includes(file.mimetype)) {
-            throw new InternalServerErrorException('Tipo de archivo no permitido. Solo imágenes JPEG, PNG y GIF.');
-          }
-    
-          const maxFileSize = 5 * 1024 * 1024; 
-          if (file.size > maxFileSize) {
-            throw new InternalServerErrorException('El archivo excede el tamaño máximo permitido de 5 MB.');
-          }
-    
-          const result = await new Promise<any>((resolve, reject) => {
-            const upload = cloudinary.uploader.upload_stream(
-              (error, result) => {
-                if (error) {
-                  return reject(error);
-                }
-                resolve(result);
-              },
-            );
-    
-            const fileStream = Readable.from(file.buffer);
-            fileStream.pipe(upload);
-          });
-    
-          return result.secure_url;
-    
-        } catch (error) {
-          throw new InternalServerErrorException(`Error al cargar la imagen: ${error.message}`);
+  async upLoadImgs(files: Express.Multer.File | Express.Multer.File[]): Promise<string[]> {
+    try {
+     
+      if (!Array.isArray(files)) {
+        files = [files];
+      }
+  
+      const allowedMimeTypes = ['image/jpeg', 'image/png', 'image/gif'];
+      const maxFileSize = 5 * 1024 * 1024; 
+  
+      for (const file of files) {
+        if (!allowedMimeTypes.includes(file.mimetype)) {
+          throw new InternalServerErrorException(`Tipo de archivo no permitido (${file.originalname}). Solo imágenes JPEG, PNG y GIF.`);
+        }
+        if (file.size > maxFileSize) {
+          throw new InternalServerErrorException(`El archivo ${file.originalname} excede el tamaño máximo permitido de 5 MB.`);
         }
       }
+  
+    
+      const uploadPromises = files.map(file =>
+        new Promise<string>((resolve, reject) => {
+          const upload = cloudinary.uploader.upload_stream(
+            (error, result) => {
+              if (error) {
+                return reject(error);
+              }
+              resolve(result.secure_url);
+            },
+          );
+  
+          const fileStream = Readable.from(file.buffer);
+          fileStream.pipe(upload);
+        })
+      );
+  
+      const results = await Promise.all(uploadPromises);
+      return results; 
+    } catch (error) {
+      throw new InternalServerErrorException(`Error al cargar las imágenes: ${error.message}`);
+    }
+  }
+  
 }
