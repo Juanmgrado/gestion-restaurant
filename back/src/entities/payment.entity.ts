3752eba23e9711dacc33c34f5ae05fd038cc48cb
Juanmgrado <jumi.rc@hotmail.com>
2024-11-24 19:53:35 +0100

    entidad payment creada

---
+++ back/src/entities/payment.entity.ts
@@ -0,0 +1,69 @@
+import {
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+    OneToOne,
+    JoinColumn,
+    ManyToOne,
+  } from 'typeorm';
+  import { ApiProperty } from '@nestjs/swagger';
+  import { Reservation } from './reservation.entity';
+  import { User } from './user.entity';
+  
+  
+  @Entity('Payments')
+  export class Payment {
+    @ApiProperty({
+      example: 'e5bdda8e-3da4-4a5f-b9ea-6239c73222f4',
+      description: 'UUID del pago',
+    })
+    @PrimaryGeneratedColumn('uuid')
+    uuid: string;
+    
+    @ApiProperty({ example: '2024-05-08', description: 'fecha en la que se realizó el pago' })
+    @Column()
+    date: Date;
+  
+    @ApiProperty({ example: 'ID:34323', description: 'ID de la orden de pago generada por paypal' })
+    @Column({nullable: true})
+    orderId: string;
+  
+    @ApiProperty({ example: 'USD', description: 'Moneda en la cual se hizo el pago' })
+    @ApiProperty({
+      example: '2024-09-20T15:00:00Z',
+      description: 'Fecha del pago',
+    })
+    @Column({nullable: true})
+    currency: string;
+  
+    @ApiProperty({ example: 'locolindo@paypal.com', description: 'Email de la cuenta con la que se realizó el pago' })
+    @Column({nullable: true})
+    payerEmail: string;
+  
+    @ApiProperty({ example: 'locolindo@paypal.com', description: 'Id de la cuenta con la que el pagador realizó el pago' })
+    @Column({nullable: true})
+    payerId: string
+  
+    @ApiProperty({
+      example: 'pending',
+      description: 'Estado del pago',
+    })
+    @Column( {default:'pendding'} )
+    status: string;
+  
+    @ApiProperty({
+      example: 100.00,
+      description: 'Total del pago realizado',
+  })
+    @Column('decimal', { precision: 10, scale: 2 })
+    total: number;
+  
+    @OneToOne(()=> Reservation, (reservation) => reservation.payment)
+    @JoinColumn()
+    reservation: Reservation
+  
+    @ManyToOne(()=> User, (user)=> user.payments)
+    @JoinColumn()
+    user: User
+  }
+  
