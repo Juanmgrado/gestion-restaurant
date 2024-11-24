2bcb0f9a7d2f6107cf1b32f5912e9374b93ea9be
Juanmgrado <jumi.rc@hotmail.com>
2024-11-24 16:07:52 +0100

    asd

--- back/src/decorators/dayCheck.decorator.ts
+++ back/src/decorators/dayCheck.decorator.ts
@@ -1,25 +1,25 @@
-import { registerDecorator, ValidationOptions, ValidationArguments } from 'class-validator';
+// import { registerDecorator, ValidationOptions, ValidationArguments } from 'class-validator';
 
-export function IsDateNotPassed(validationOptions?: ValidationOptions) {
-  return function (object: any, propertyName: string) {
-    registerDecorator({
-      name: 'isDateInFuture',
-      target: object.constructor,
-      propertyName: propertyName,
-      options: validationOptions,
-      validator: {
-        validate(value: any, args: ValidationArguments) {
-          if (value instanceof Date) {
-            const now = new Date();
-            now.setHours(0, 0, 0, 0);  
-            return value >= now;  
-          }
-          return false;
-        },
-        defaultMessage(args: ValidationArguments) {
-          return 'La fecha de la reserva no puede ser menor al día de hoy';
-        },
-      },
-    });
-  };
-}
+// export function IsDateNotPassed(validationOptions?: ValidationOptions) {
+//   return function (object: any, propertyName: string) {
+//     registerDecorator({
+//       name: 'isDateInFuture',
+//       target: object.constructor,
+//       propertyName: propertyName,
+//       options: validationOptions,
+//       validator: {
+//         validate(value: any, args: ValidationArguments) {
+//           if (value instanceof Date) {
+//             const now = new Date();
+//             now.setHours(0, 0, 0, 0);  
+//             return value >= now;  
+//           }
+//           return false;
+//         },
+//         defaultMessage(args: ValidationArguments) {
+//           return 'La fecha de la reserva no puede ser menor al día de hoy';
+//         },
+//       },
+//     });
+//   };
+// }
