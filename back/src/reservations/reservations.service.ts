56e38e6d429a423f983c9f450593fcb0c992ddd8
Juanmgrado <jumi.rc@hotmail.com>
2024-11-27 00:24:05 +0100

    bloque pesismista para reservas

--- back/src/reservations/reservations.service.ts
+++ back/src/reservations/reservations.service.ts
@@ -6,7 +6,7 @@ import { IStatus, Reservation } from 'src/entities/reservation.entity';
 import { Table } from 'src/entities/table.entity';
 import { User } from 'src/entities/user.entity';
 import { NodemailerService } from 'src/nodemailer/nodemailer.service';
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 
 @Injectable()
 export class ReservationsService {
@@ -17,45 +17,51 @@ export class ReservationsService {
         private readonly tableRespository: Repository <Table>,
         @InjectRepository(Reservation)
         private readonly reservationsRespository: Repository <Reservation>,
-        private readonly nodemailerService: NodemailerService
+        private readonly nodemailerService: NodemailerService,
+        private readonly datasource: DataSource
     ){}
-
 
     async createReservation(newReservation: CreateReservationDto, userUuid: string): Promise<ReturnedReservation | null> {
         const { startTime, tableNumber, day, guests } = newReservation;
 
-        const foundUser = await this.userRepository.findOne({ where: { uuid: userUuid } });
-        if (!foundUser) throw new NotFoundException('Usuario no encontrado');
+        return await this.datasource.transaction(async (manager) =>{
 
-        const foundTable = await this.tableRespository.findOne({ where: { tableNumber } });
-        if (!foundTable) throw new NotFoundException('Número de mesa incorrecto');
-
+            const foundUser = await manager.getRepository(User).findOne({
+                 where: { uuid: userUuid } });
+                if (!foundUser) throw new NotFoundException('Usuario no encontrado');
+            
+            const foundTable = await manager.getRepository(Table).findOne({ 
+                where: { tableNumber },
+                lock: {mode: 'pessimistic_write'} });
+                    if (!foundTable) throw new NotFoundException('Número de mesa incorrecto');
+            
         
-        const createdReservation = this.reservationsRespository.create({
-            day,
-            startTime,
-            guests,
-            tableNumber,
-            table: foundTable
-        });
+            const createdReservation = await manager.getRepository(Reservation).create({
+                day,
+                startTime,
+                guests,
+                tableNumber,
+                table: foundTable
+            });
 
-        await this.reservationsRespository.save(createdReservation);
-        
-        createdReservation.user = foundUser;
-            await this.reservationsRespository.save(createdReservation)
-
-        const returnedReservation:ReturnedReservation  = {
-            reservIdentification: createdReservation.uuid,
-            day: createdReservation.day,
-            startTime: createdReservation.startTime,
-            guest: createdReservation.guests,
-            reservationWorth: createdReservation.reservationWorth,
-            tableNumber: createdReservation.tableNumber,
-            status: createdReservation.status
-        };
-        
-        await this.nodemailerService.reservationMail(foundUser.email, returnedReservation)
-        return returnedReservation;
+            await manager.getRepository(Reservation).save(createdReservation);
+            
+            createdReservation.user = foundUser;
+            await manager.getRepository(Reservation).save(createdReservation)
+            
+            const returnedReservation:ReturnedReservation  = {
+                reservIdentification: createdReservation.uuid,
+                day: createdReservation.day,
+                startTime: createdReservation.startTime,
+                guest: createdReservation.guests,
+                reservationWorth: createdReservation.reservationWorth,
+                tableNumber: createdReservation.tableNumber,
+                status: createdReservation.status
+            };
+            
+            await this.nodemailerService.reservationMail(foundUser.email, returnedReservation)
+            return returnedReservation;
+        })
     }
 
     async actualiceStatus(reservationUuid: string, newStatus: IStatus): Promise< string | null >{
